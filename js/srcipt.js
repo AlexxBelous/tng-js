f736faa852a61dd1c9b6da1cf85a4e44b62e0d6b
AlexxBelous <connectionwithme1@gmail.com>
2022-12-25 17:16:25 +0200

    f

--- js/srcipt.js
+++ js/srcipt.js
@@ -166,17 +166,54 @@
 
 // // Создать div
 // const div = document.createElement('div');
+
+
+
+
 // // Добавить к нему класс wrapper
 // div.classList.add('wrapper');
-// // Поместить его внутрь тэга body
+
+
+
+
+
+
+// // Получить в переменную тэг body.
 // const body = document.body;
+
+
+
+
+
+// // Поместить его внутрь тэга body
 // body.appendChild(div);
+
+
+
+
+
+
 // // Создать заголовок H1 "DOM (Document Object Model)"
 // const header = document.createElement('h1');
 // header.textContent = 'DOM (Document Object Model)';
 
+
+
+
+
+
+
+
 // // Добавить H1 перед DIV с классом wrapper
-// div.insertAdjacentElement('beforebegin', header)
+// div.insertAdjacentElement('beforebegin', header);
+
+
+
+
+
+
+
+
 // // Создать список <ul></ul>
 // // Добавить в него 3 элемента с текстом "один, два, три"
 
@@ -188,36 +225,101 @@
 //     </ul>
 // `;
 
+
+
+
+
+
+
 // // Поместить список внутрь элемента с классом wrapper
 // div.innerHTML = ul;
-// // =================================================
+
+
+
+
+
+
+
+
 // // Создать изображение
 // const img = document.createElement('img');
-// // Добавить следующие свойства к изображению
-// // 1. Добавить атрибут source
+
+
+
+
+
+
+// // Добавить следующие свойства к изображению:
+// // 1.атрибут source
 // img.src = 'https://picsum.photos/240';
-// // 2. Добавить атрибут width со значением 240
+// // 2.атрибут width со значением 240
 // img.width = 240;
-// // 3. Добавить класс super
+// // 3.класс super
 // img.classList.add('super');
-// // 4. Добавить свойство alt со значением "Super Man"
+// // 4.свойство alt со значением "Super Man"
 // img.alt = 'Super Man';
+
+
+
+
+
+
 // // Поместить изображение внутрь элемента с классом wrapper
 // div.appendChild(img)
+
+
+
+
+
+
+
 // // Используя HTML строку, создать DIV с классом 'pDiv' + c 2-мя параграфами
 // const elemHTML = `
 // <div class='pDiv'>
 //     <p>Параграф 1</p>
 //     <p>Параграф 2</p>
-// </div>`
+// </div>`;
+
+
+
+
+
+
+
 // // Поместить этот DIV до элемента <ul></ul>
 // const ulList = div.querySelector('ul');
 // ulList.insertAdjacentHTML('beforebegin', elemHTML)
+
+
+
+
+
+
+
+
+
 // // Добавить для 2-го параграфа класс text
 // const pDiv = document.querySelector('.pDiv');
 // pDiv.children[1].classList.add('text');
+
+
+
+
+
+
+
+
+
+
 // // Удалить 1-й параграф
 // pDiv.firstElementChild.remove();
+
+
+
+
+
+
+
 
 // // Создать функцию generateAutoCard, 
 // // которая принимает 3 аргумента: brand, color, year
@@ -233,15 +335,38 @@
 //         </div>
 //     `;
 // }
+
+
+
+
+
+
+
 // // Функция должна возвращать разметку HTML:
 // // <div class="autoCard">
 // //   <h2>BRAND YEAR</h2>
 // //   <p>Автомобиль BRAND - YEAR года. Возраст авто - YEARS лет.</p>
 // // </div>
 
+
+
+
+
+
+
+
 // // Создать новый DIV с классом autos
 // const carsDiv = document.createElement('div');
 // carsDiv.classList.add('autos');
+
+
+
+
+
+
+
+
+
 
 // // Создать 3 карточки авто, используя функцию generateAutoCard
 // const carsList = [
@@ -250,19 +375,51 @@
 //     {brand: 'Nissan', year: 2012, color: 'Черный'},
 // ]
 
+
 // const carsHTML = carsList.map(car => {
 //     return generateAutoCard(car.brand, car.color, car.year);
 // }).join('');
 
+
+
+
+
+
+
+
+
+
+
+
 // // Поместить эти 3 карточки внутрь DIV с классом autos
 // carsDiv.innerHTML = carsHTML;
+
+
+
+
+
+
+
+
 // // Поместить DIV c классом autos на страницу DOM - до DIV с классом wrapper
 // div.insertAdjacentElement('beforebegin', carsDiv);
+
+
+
+
+
+
+
 // // Добавить кнопку Удалить на каждую карточку авто
 
 // // При клике на кнопку - удалять карточку из структуры DOM
 // // 1. Выбрать все кнопки
 // const buttons = document.querySelectorAll('.btn');
+
+
+
+
+
 
 //     // 2. Создать функцию удаления
 //     function handleClick(e) {
@@ -270,6 +427,11 @@
 //         currentButton.closest('.autoCard').remove();
 //         // console.log(currentButton.parentElement)
 //     }
+
+
+
+
+    
 //     // 3. Использовать цикл - чтобы повесить обработчик события на каждую кнопку
 //     buttons.forEach(button => {
 //         button.addEventListener('click', handleClick)
